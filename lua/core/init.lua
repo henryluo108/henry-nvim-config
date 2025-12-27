6590132d6f33160e4f4355d9119a01279709b85a
henryluo <henryluo108@gmail.com>
2025-12-27 16:08:21 +0800

    chore: fix checkhealth issues and update plugins
    - Disable Perl and Ruby providers in lua/core/init.lua to suppress checkhealth warnings
    - Update lazy-lock.json with latest plugin versions

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -41,6 +41,8 @@ vim.g.loaded_netrw             = 1
 vim.g.loaded_netrwPlugin       = 1
 vim.g.loaded_tutor_mode_plugin = 1
 vim.g.loaded_remote_plugins    = 1
+vim.g.loaded_perl_provider     = 0
+vim.g.loaded_ruby_provider     = 0
 
 -- Load Lazy.nvim plugin manager
 require("configs.lazy").config()
